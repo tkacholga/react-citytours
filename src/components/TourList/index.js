a6a12137f84eaedaf1e1222a7216fac8e2f8d0ab
Olga Tkach <tkacholya@gmail.com>
2019-02-13 14:43:37 -0500

    Removing a tour from the screen

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -8,13 +8,23 @@ export default class TourList extends Component {
     tours: tourData
   };
 
+  removeTour = id => {
+    const { tours } = this.state;
+    const sortedTours = tours.filter(tour => tour.id !== id);
+    this.setState({
+      tours: sortedTours
+    });
+  };
+
   render() {
     const { tours } = this.state;
 
     return (
       <section className="tourlist">
         {tours.map(tour => {
-          return <Tour key={tour.id} tour={tour} />;
+          return (
+            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+          );
         })}
       </section>
     );
